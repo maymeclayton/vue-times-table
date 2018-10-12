458ed34bea374d7b9a92f97fd2f0100a91c3a011
Mayme Clayton <mayme.clayton@gmail.com>
2018-10-12 16:33:53 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+var app = new Vue ({
+
+  el: '#app',
+
+  data: {
+    rowInput: 4,
+    colInput: 4,
+
+
+  },
+
+  methods:{
+
+    fill (){
+    console.log("hello world");
+
+    //column value * row value
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+})
